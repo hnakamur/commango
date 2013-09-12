2762e0a70a271c7af3d59ec6ad12d01ef8ef1ccd
Hiroaki Nakamura <hnakamur@gmail.com>
2013-09-13 05:17:48 +0900

    Use Marshal and Unmarshal instead of Encode and Decode.

--- jsonutil/jsonutil.go
+++ jsonutil/jsonutil.go
@@ -1,21 +1,18 @@
 package jsonutil
 
 import (
-	"bytes"
 	"encoding/json"
-	"strings"
 )
 
 func Encode(v interface{}) (string, error) {
-	var buf bytes.Buffer
-    err := json.NewEncoder(&buf).Encode(v)
+    bytes, err := json.Marshal(v)
     if err != nil {
         return "", err
     }
-	return strings.TrimRight(buf.String(), "\n"), nil
+	return string(bytes), nil
 }
 
 func Decode(s string) (v interface{}, err error) {
-	err = json.NewDecoder(bytes.NewReader([]byte(s))).Decode(&v)
+	err = json.Unmarshal([]byte(s), &v)
 	return
 }
